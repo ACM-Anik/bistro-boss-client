3201919394182cade5864c77119d4f7587d93a0d
ACM-Anik <anikmojumder5252@gmail.com>
2023-05-28 04:59:33 +0600

    first commit

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,58 @@
+import { Helmet } from "react-helmet-async";
+import menuImg from '../../../assets/menu/banner3.jpg';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import saladImg from '../../../assets/menu/salad-bg.jpg';
+import soupImg from '../../../assets/menu/soup-bg.jpg';
+import Cover from "../../Shared/Cover/Cover";
+import useMenu from "../../../Hooks/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTltle";
+import MenuCategory from "../MenuCategory/MenuCategory";
+
+
+const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+    return (
+        <div>
+            <Helmet>
+                <title>Menu | Bistro Boss</title>
+            </Helmet>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            {/* Main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* Offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* Dessert menu items */}
+            <MenuCategory
+                items={desserts}
+                title="dessert"
+                img={dessertImg}
+            ></MenuCategory>
+            {/* Pizza menu items */}
+            <MenuCategory
+                items={pizza}
+                title="pizza"
+                img={pizzaImg}
+            ></MenuCategory>
+            {/* Salad menu items */}
+            <MenuCategory
+                items={salad}
+                title="salad"
+                img={saladImg}
+            ></MenuCategory>
+            {/* Soup menu items */}
+            <MenuCategory
+                items={soup}
+                title="soup"
+                img={soupImg}
+            ></MenuCategory>
+        </div>
+    );
+};
+
+export default Menu;
