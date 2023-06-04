8c56b56030b99468dd54f13469d7ac6085c4252b
ACM-Anik <anikmojumder5252@gmail.com>
2023-06-05 04:15:25 +0600

    recharts added

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -9,6 +9,7 @@ const MyCart = () => {
     console.log(cart);
     // how does reduce work!!!
     const total = cart.reduce((sum, item) => item.price + sum, 0);
+    const price = parseFloat(total.toFixed(2));
 
     const handleDelete = item => {
         Swal.fire({
@@ -46,7 +47,7 @@ const MyCart = () => {
             </Helmet>
             <div className="uppercase font-semibold h-[60px] flex justify-evenly items-center">
                 <h3 className="text-3xl">Total Items: {cart.length}</h3>
-                <h3 className="text-3xl">Total Price: ${total}</h3>
+                <h3 className="text-3xl">Total Price: ${price}</h3>
                 <Link to="/dashboard/payment"><button className="btn btn-warning btn-sm">PAY</button></Link>
             </div>
             <div className="overflow-x-auto w-full">
